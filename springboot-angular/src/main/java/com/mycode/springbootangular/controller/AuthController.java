6acffcf9366512d27216b257be1610228edd5da5
jay <jayshah5109@gmail.com>
2022-10-05 21:27:48 +0530

    added code to call authService from AuthController

--- springboot-angular/src/main/java/com/mycode/springbootangular/controller/AuthController.java
+++ springboot-angular/src/main/java/com/mycode/springbootangular/controller/AuthController.java
@@ -1,6 +1,10 @@
 package com.mycode.springbootangular.controller;
 
 import com.mycode.springbootangular.dto.RegisterRequest;
+import com.mycode.springbootangular.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@AllArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
 
     @PostMapping("/signup")
-    public void signUp(@RequestBody RegisterRequest registerRequest) {
-
+    public ResponseEntity<String> signUp(@RequestBody RegisterRequest registerRequest) {
+        authService.signUp(registerRequest);
+        return new ResponseEntity<>("User registration completed successfully", HttpStatus.OK);
     }
 }
