0888fd5a943cd1fdbfc7ef21bd4c33251b8f5f6e
jay <jayshah5109@gmail.com>
2022-10-05 18:14:39 +0530

    added mailService class, NotificationEmail model class and SpringRedditException class

---
+++ springboot-angular/src/main/java/com/mycode/springbootangular/exceptions/SpringRedditException.java
@@ -0,0 +1,9 @@
+package com.mycode.springbootangular.exceptions;
+
+import org.springframework.mail.MailException;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String s, MailException e) {
+        super(e);
+    }
+}
