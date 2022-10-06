95c14427b15accf0e1e825e26da209889bf12e51
jay <jayshah5109@gmail.com>
2022-10-06 17:34:31 +0530

    added @async annotation for async processing of requests for faster API response

--- springboot-angular/src/main/java/com/mycode/springbootangular/SpringbootAngularApplication.java
+++ springboot-angular/src/main/java/com/mycode/springbootangular/SpringbootAngularApplication.java
@@ -2,8 +2,10 @@ package com.mycode.springbootangular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringbootAngularApplication {
 
 	public static void main(String[] args) {
